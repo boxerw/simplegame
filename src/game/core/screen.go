10a31d3e0d9838fecdb8375e621274bc3928265d
boxerw <boxerw@163.com>
2020-12-14 15:23:30 +0800

    修改代码

--- src/game/core/screen.go
+++ src/game/core/screen.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"fmt"
+	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
 
@@ -14,6 +15,7 @@ func NewScreen(ctx *Context, logic ...Logic) *Screen {
 
 type Screen struct {
 	_ObjectBase
+	drawCount int
 }
 
 func (screen *Screen) Destroy() {
@@ -26,17 +28,37 @@ func (screen *Screen) Update() {
 		return
 	}
 
+	if screen.drawCount > 0 {
+		if err := termbox.Flush(); err != nil {
+			panic(fmt.Sprintf("termbox Flush failed, %v", err))
+		}
+	}
+
 	screen.updateObject()
 }
 
 func (screen *Screen) init() {
 	if err := termbox.Init(); err != nil {
-		panic(fmt.Sprintf("termbox init failed, %v", err))
+		panic(fmt.Sprintf("termbox Init failed, %v", err))
 	}
 
-	termbox.Clear(termbox.RGBToAttribute(154, 22, 30), termbox.RGBToAttribute(0, 0, 0))
+	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
+	termbox.SetOutputMode(termbox.OutputRGB)
 }
 
 func (screen *Screen) shut() {
 	termbox.Close()
+}
+
+func (screen *Screen) Draw(x, y int, fg, bg termbox.Attribute, text string) {
+	if len(text) <= 0 {
+		return
+	}
+
+	for _, c := range text {
+		termbox.SetCell(x, y, c, fg, bg)
+		x += runewidth.RuneWidth(c)
+	}
+
+	screen.drawCount++
 }
--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module simple
 go 1.15
 
 require (
+	github.com/mattn/go-runewidth v0.0.9
 	github.com/nsf/termbox-go v0.0.0-20201124104050-ed494de23a00
 	github.com/ungerik/go3d v0.0.0-20200217190927-d82aececefe9
 )
