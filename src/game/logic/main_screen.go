bc9f118036ef0725926f03b50952131a2461f0e8
boxerw <boxerw@163.com>
2020-12-15 13:22:30 +0800

    修改代码

--- src/game/logic/main_screen.go
+++ src/game/logic/main_screen.go
@@ -4,6 +4,7 @@ import (
 	"github.com/nsf/termbox-go"
 	"math/rand"
 	. "simple/game/core"
+	"time"
 )
 
 type cache struct {
@@ -23,20 +24,46 @@ func (logic *MainScreen) Name() string {
 func (logic *MainScreen) Init(object Object) {
 	logic.screen = object.(*Screen)
 	logic.screen.GetContext().SetValue("MainScreen", logic.screen)
+	rand.Seed(time.Now().UnixNano())
 }
 
 func (logic *MainScreen) Shut() {
 }
 
 func (logic *MainScreen) Update() {
-	if len(logic.caches) < 300 {
-		w, h := termbox.Size()
-		c := cache{
-			x:     rand.Intn(w) / 2 * 2,
-			y:     rand.Intn(h),
-			color: termbox.Attribute(rand.Int()%256) + 1,
+	if len(logic.caches) < 1024 {
+		if len(logic.caches) <= 0 {
+			w, h := termbox.Size()
+			c := cache{
+				x:     rand.Intn(w) / 2 * 2,
+				y:     rand.Intn(h),
+				color: termbox.Attribute(rand.Int()%256) + 1,
+			}
+			logic.caches = append(logic.caches, c)
+		} else {
+			c := cache{
+				x:     logic.caches[len(logic.caches)-1].x + rand.Intn(3) - 1,
+				y:     logic.caches[len(logic.caches)-1].y + rand.Intn(3) - 1,
+				color: termbox.Attribute(rand.Int()%256) + 1,
+			}
+			w, h := termbox.Size()
+			if c.x < 0 {
+				c.x = w - 1
+			} else if c.x >= w {
+				c.x = 0
+			}
+			if c.y < 0 {
+				c.y = h - 1
+			} else if c.y >= h {
+				c.y = 0
+			}
+
+			c.x = c.x / 2 * 2
+
+			logic.caches = append(logic.caches, c)
 		}
-		logic.caches = append(logic.caches, c)
+	} else {
+		logic.caches = nil
 	}
 
 	for _, c := range logic.caches {
